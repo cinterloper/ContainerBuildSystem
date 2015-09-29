53ed60ad611557bd8c7c18cd3fc1f1e0e2544adf
grant <grant@iowntheinter.net>
2015-09-28 18:13:57 -0700

    cleanup

--- .util/build.sh
+++ .util/build.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 source .cfg/*
-
-COMPONENT=$1
-
+usage () {
+	echo ""
+	echo "PARAMETERS:"
+	echo "  -c=COMPONENT        image to build"
+	echo "  -k=KEY       	    include this ssh key."
+	echo "  -h                  This message."
+	exit 0
+}
+while [ $# -gt 0 ]; do
+ case ${1} in
+      -c=*|--c=*) COMPONENT="$(echo ${1} | awk -F= '{print $2;}')" ;;
+      -k=*|--k=*) KEY="$(echo ${1} | awk -F= '{print $2;}')" ;;
+      -h|--h|?) usage ;;
+      *) usage ;;
+     esac
+ shift
+done
 cp $HOME/.ssh/id_rsa $COMPONENT/id.rsa
 cd $COMPONENT
 docker build -t $BASEREPO/$(cat meta.json | jq -r .tag) .
-rm id.rsa
+if [[ -f id.rsa ]]; then
+  rm id.rsa
+fi 
 if [[ -e init.sh ]]
 then
  bash init.sh
--- .util/launch.sh
+++ .util/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #pass in the name of the docker shard folder you want to run
+source .cfg/*
 
-BASEREPO='camnet'
 EXTRARGS=$(cat $1/meta.json | jq -r .extra | grep -v null)  
 
 docker run -d $(bash .util/ports.sh $1/ports.json)  $(bash .util/binds.sh $1/binds.json) $EXTRARGS "$BASEREPO/$1" 
